4c778b76b12f47dd7218769f4d41994c19042df6
Pedro de Oliveira <falsovsky@gmail.com>
2012-01-15 16:08:30 +0000

    Minimum autoconf lowered to 2.59

--- regen.sh
+++ regen.sh
@@ -4,8 +4,8 @@ if [ $OS = "OpenBSD" ]; then
 	echo "OpenBSD detected"
 	if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
 		echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
-		echo "Install at least autoconf-2.64 and automake-1.9.6p8 and run:"
-		echo "export AUTOCONF_VERSION=2.64"
+		echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
+		echo "export AUTOCONF_VERSION=2.59"
 		echo "export AUTOMAKE_VERSION=1.9"
 		exit 1
 	fi
