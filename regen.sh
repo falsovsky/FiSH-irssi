31cd8794a060f9cfdb9bbe4e734ef3d247618d29
Pedro de Oliveira <falsovsky@gmail.com>
2012-01-15 15:45:18 +0000

    Fixed issues with OpenBSD-stable

--- regen.sh
+++ regen.sh
@@ -4,9 +4,9 @@ if [ $OS = "OpenBSD" ]; then
 	echo "OpenBSD detected"
 	if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
 		echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
-		echo "Install at least autoconf-2.65 and automake-1.11.1p2 and run:"
-		echo "export AUTOCONF_VERSION=2.13"
-		echo "export AUTOMAKE_VERSION=1.11"
+		echo "Install at least autoconf-2.64 and automake-1.9.6p8 and run:"
+		echo "export AUTOCONF_VERSION=2.64"
+		echo "export AUTOMAKE_VERSION=1.9"
 		exit 1
 	fi
 	echo "Dont forget to add --with-gmp-include=/usr/local/include --with-gmp-lib=/usr/local/lib to the configure script"
