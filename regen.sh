4f2d694ef1e69c319861a17b191de7564f9ad436
Pedro de Oliveira <falsovsky@gmail.com>
2012-01-15 17:12:18 +0000

    Improve regen script

--- regen.sh
+++ regen.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-OS=`uname -s`
-if [ $OS = "OpenBSD" ]; then
-	echo "OpenBSD detected"
-	mkdir -p m4
-	if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
-		echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
-		echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
-		echo "export AUTOCONF_VERSION=2.59"
-		echo "export AUTOMAKE_VERSION=1.9"
-		exit 1
-	fi
-fi
 
-if [[ $OS = "OpenBSD" || $OS = "FreeBSD" ]]; then
-    echo "Dont forget to add --with-gmp-include=/usr/local/include --with-gmp-lib=/usr/local/lib to the configure script"
-fi
+case $(uname -s) in
+    OpenBSD)
+        echo "OpenBSD detected"
+        mkdir -p m4
+        if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
+            echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
+            echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
+            echo "export AUTOCONF_VERSION=2.59"
+            echo "export AUTOMAKE_VERSION=1.9"
+            exit 1
+        fi
+        ;;
+    OpenBSD|FreeBSD)
+        echo "Dont forget to add --with-gmp-include=/usr/local/include --with-gmp-lib=/usr/local/lib to the configure script"
+        ;;
+esac
 
 aclocal
 libtoolize --force
