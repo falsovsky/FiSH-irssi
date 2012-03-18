30298c4bf511213fc422253ca9f0ca8d2311d64f
Pedro de Oliveira <falsovsky@gmail.com>
2012-03-18 05:40:44 +0000

    Better OS support

--- regen.sh
+++ regen.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-case $(uname -s) in
-    OpenBSD)
-        echo "OpenBSD detected"
-        mkdir -p m4
-        if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
-            echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
-            echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
-            echo "export AUTOCONF_VERSION=2.59"
-            echo "export AUTOMAKE_VERSION=1.9"
-            exit 1
-        fi
-        ;;
-    OpenBSD|FreeBSD)
-        echo "Dont forget to add --with-gmp-include=/usr/local/include --with-gmp-lib=/usr/local/lib to the configure script"
-        ;;
-esac
+OS=$(uname -s)
+
+if [ "$OS" = "OpenBSD" ];
+then
+    echo "OpenBSD detected"
+    mkdir -p m4
+    if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
+        echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
+        echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
+        echo "export AUTOCONF_VERSION=2.59"
+        echo "export AUTOMAKE_VERSION=1.9"
+        exit 1
+    fi
+fi
+
+if [ "$OS" = "OpenBSD" -o "$OS" = "FreeBSD" ];
+then
+    echo "Dont forget to add --with-gmp-include=/usr/local/include --with-gmp-lib=/usr/local/lib to the configure script"
+fi
+
+if [ "$OS" = "NetBSD" ];
+then
+    echo "Dont forget to add --with-gmp-include=/usr/pkg/include --with-gmp-lib=/usr/pkg/lib to the configure script"
+fi
 
 aclocal
 libtoolize --force
