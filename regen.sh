8a4efc3041805370e0c61b9cfacfb20b8e794a36
Pedro de Oliveira <falsovsky@gmail.com>
2012-03-18 05:42:59 +0000

    Explain better

--- regen.sh
+++ regen.sh
@@ -7,7 +7,8 @@ then
     echo "OpenBSD detected"
     mkdir -p m4
     if [[ -z "$AUTOCONF_VERSION" || -z "$AUTOMAKE_VERSION" ]]; then
-        echo "Please define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
+        echo "ERROR:"
+        echo "You must define the AUTOCONF_VERSION and AUTOMAKE_VERSION environment variables"
         echo "Install at least autoconf-2.59p3 and automake-1.9.6p8 and run:"
         echo "export AUTOCONF_VERSION=2.59"
         echo "export AUTOMAKE_VERSION=1.9"
