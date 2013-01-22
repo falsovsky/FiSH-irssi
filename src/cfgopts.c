8be089bdb6f954374ba475083c6e5266efc716df
Pedro de Oliveira <falsovsky@gmail.com>
2013-01-22 00:08:52 +0000

    Now using Glib's GKeyFile for reading and writing blow.ini - http://developer.gnome.org/glib/stable/glib-Key-value-file-parser.html

--- src/cfgopts.c
+++ src/cfgopts.c
@@ -1,192 +1,50 @@
 #include "cfgopts.h"
-
-static unsigned char line[1000], line_bak[1000];
+#include <glib.h>
 
 int GetPrivateProfileString(const char *section, const char *key, const char *default_value, char *buffer, int buflen, const char *filepath)
 {
-    int len=0;
-    FILE *file;
-    char *fptr, *key_tok, *value_tok;
-    unsigned char INIsection[strlen(section)+4];
+    GKeyFile *key_file;
+    GError *error;
 
-    if(strchr(section, '[')!=NULL || strchr(section, ']')!=NULL) goto use_default_value; // bad section
+    key_file = g_key_file_new();
+    error = NULL;
 
-    file=fopen(filepath, "r");
-    if(file==NULL) goto use_default_value;
+    g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, &error);
+    if (error != NULL) goto default_value;
 
-    buffer[0]='\0';
-    sprintf((char *)INIsection, "[%s]", section);
-
-    do
-    {
-        fptr=fgets((char *)line, sizeof(line)-2, file);
-    }
-    while(strncasecmp((char *)line, (char *)INIsection, strlen((char *)INIsection)) && !feof(file));
-
-    do
-    {
-        fptr=fgets((char *)line, sizeof(line)-2, file);
-        if(fptr==NULL) break;
-
-        if(*line=='#' || *line==';' || *line=='/' || *line=='[') continue;    // skip comments
-
-        key_tok=strtok((char *)line, "=\n\r");   // get first token (key)
-        if(key_tok)
-        {
-            value_tok=strtok(NULL, "=\n\r"); // get actual value information
-            if(!value_tok) continue;
-            if(!strcasecmp(key_tok, key))  // got a match?
-            {
-                snprintf(buffer, buflen, "%s", value_tok);
-                break;
-            }
-        }
-    }
-    while(fptr!=NULL && *line!='[');
-
-    fclose(file);
-
-    len=strlen(buffer);
-    if(*buffer==0x22 && buffer[len-1]==0x22)	// kill ""
-    {
-        memmove(buffer, buffer+1, len-2);
-        buffer[len-2]='\0';
+    if (g_key_file_get_string(key_file, section, key, &error) != NULL) {
+        snprintf(buffer, buflen, "%s",  g_key_file_get_string(key_file, section, key, &error));
+        return strlen(buffer);
     }
 
-use_default_value:
-    if(len==0) snprintf(buffer, buflen, "%s", default_value);
+    default_value:
+    if (error != NULL) {
+        snprintf(buffer, buflen, "%s", default_value);
+    }
 
     return strlen(buffer);
 }
 
 int WritePrivateProfileString(const char *section, const char *key, const char *value, const char *filepath)
 {
-    int section_not_found=1;
-    FILE *infile, *outfile;
-    char *fptr, *tok;
-    unsigned char INIsection[strlen(section)+4];
+    GKeyFile *key_file;
+    GError *error;
+    FILE *outfile;
 
-    sprintf((char *)INIsection, "[%s]", section);
+    key_file = g_key_file_new();
+    error = NULL;
+    g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, &error);
 
-    infile=fopen(filepath, "r");
-    if(infile==NULL)
-    {	// file not found, creating new now
-        if(*value=='\0') return 1;	// nothing to write
-        outfile=fopen(filepath, "w");
-        if(outfile==NULL) return -1;
-        if(fprintf(outfile, "%s\n%s=%s\n", INIsection, key, value) < 0)
-        {
-            fclose(outfile);
-            return -1;
-        }
-        fclose(outfile);
-        return 1;
+    if (error != NULL) {
+        key_file = g_key_file_new();
     }
 
-    outfile=fopen(tempPath,"w");
-    if(outfile==NULL)
-    {
-        fclose(infile);
-        return -1;
-    }
+    g_key_file_set_string(key_file, section, key, value);
 
-    do
-    {
-        fptr=fgets((char *)line, sizeof(line)-2, infile);
-        if(fptr)
-        {
-            if(fprintf(outfile, "%s", line) < 0) goto fprint_error;
-            section_not_found=strncasecmp((char *)line, (char *)INIsection, strlen((char *)INIsection));
-        }
-    }
-    while(section_not_found && !feof(infile));
-
-    if(feof(infile))
-    {
-        if(*value)
-        {	// if *value==0, we are deleting
-            if(section_not_found)
-            {
-                if(fprintf(outfile,"\n\n%s", INIsection) < 0) goto fprint_error;
-            }
-            if(fprintf(outfile, "\n%s=%s\n", key, value) < 0) goto fprint_error;
-        }
-    }
-    else
-    {
-        do
-        {
-            fptr=fgets((char *)line, sizeof(line)-2, infile);
-            if(fptr==NULL)
-            {	// eof reached, lets write it
-                if(*value)
-                {	// if *value==0, we are deleting
-                    if(fprintf(outfile, "%s=%s\n", key, value) < 0) goto fprint_error;
-                }
-                break;
-            }
-
-            if(*line=='#' || *line=='\r' || *line=='\n' || *line==';' || *line=='/')
-            {
-                if(fprintf(outfile, "%s", line) < 0) goto fprint_error;
-                continue;  // skip comments
-            }
-            else if(*line=='[')
-            {	// new section found, last section was empty
-                if(*value)
-                {
-                    if(fprintf(outfile, "%s=%s\n\n", key, value) < 0) goto fprint_error;
-                }
-
-                if(fprintf(outfile, "%s", line) < 0) goto fprint_error;
-                do
-                {	// write the rest of the file
-                    fptr=fgets((char *)line, sizeof(line)-2, infile);
-                    if(fptr)
-                    {
-                        if(fprintf(outfile, "%s", line) < 0) goto fprint_error;
-                    }
-                }
-                while(!feof(infile));
-                break;
-            }
-
-            strcpy((char *)line_bak, (char *)line);
-            tok=strtok((char *)line, "=\n\r");  // get first token
-            if(tok)
-            {
-                if(!strcasecmp(tok, key)) // got a match?
-                {
-                    if(*value)
-                    {
-                        if(fprintf(outfile, "%s=%s\n", key, value) < 0) goto fprint_error;
-                    }
-                    do
-                    {	// write the rest of the file
-                        fptr=fgets((char *)line,sizeof(line)-2, infile);
-                        if(fptr)
-                        {
-                            if(fprintf(outfile, "%s", line) < 0) goto fprint_error;
-                        }
-                    }
-                    while(!feof(infile));
-                    break;
-                }
-                if(fprintf(outfile, "%s", line_bak) < 0) goto fprint_error;
-            }
-        }
-        while(fptr);
-    }
-
-    fclose(infile);
+    outfile=fopen(filepath, "w");
+    if (outfile == NULL) return -1;
+    fprintf(outfile, "%s", g_key_file_to_data(key_file, NULL, NULL));
     fclose(outfile);
-
-    if(rename(tempPath, filepath)!=0) return -1;
 
     return 1;
-
-fprint_error:
-    fclose(infile);
-    fclose(outfile);
-    return -1;
 }
--- src/cfgopts.h
+++ src/cfgopts.h
@@ -4,5 +4,3 @@
 
 int WritePrivateProfileString(const char *section, const char *key, const char *value, const char *filepath);
 int GetPrivateProfileString(const char *section, const char *key, const char *default_value, char *buffer, int buflen, const char *filepath);
-
-extern char tempPath[];
