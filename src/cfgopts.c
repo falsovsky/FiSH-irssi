dd842b3d95890e3158c1f413d8997fe5e300eb16
Pedro de Oliveira <falsovsky@gmail.com>
2013-01-28 22:33:11 +0000

    Optimizations and fix memory leaks

--- src/cfgopts.c
+++ src/cfgopts.c
@@ -4,59 +4,59 @@
 int GetPrivateProfileString(const char *section, const char *key, const char *default_value, char *buffer, int buflen, const char *filepath)
 {
     GKeyFile *key_file;
-    GError *error;
+    GError *error = NULL;
     gchar *value = NULL;
 
     key_file = g_key_file_new();
-    error = NULL;
-
-    g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, &error);
 
     // If file was read OK...
-    if (error == NULL) {
+    if ((int) g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, NULL)==1) {
         // If the record was found...
         value = g_key_file_get_string(key_file, section, key, &error);
         if (value != NULL && error == NULL) {
-            strncpy(buffer, g_key_file_get_string(key_file, section, key, NULL), buflen);
+            strncpy(buffer, value, (size_t)buflen);
         }
     }
 
     g_free(value);
+    g_free(key_file);
 
     // In case of any error...
     if (error != NULL) {
-        strncpy(buffer, default_value, buflen);
+        strncpy(buffer, default_value, (size_t)buflen);
     }
 
-    return strlen(buffer);
+
+    return (int) strlen(buffer);
 }
 
 int WritePrivateProfileString(const char *section, const char *key, const char *value, const char *filepath)
 {
     GKeyFile *key_file;
-    GError *error;
-    FILE *outfile;
-    gsize length;
+    GError *error = NULL;
+    FILE *outfile = NULL;
+    gsize length = 0;
     gchar *config = NULL;
 
     key_file = g_key_file_new();
-    error = NULL;
 
-    g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, NULL);
-
-    g_key_file_set_string(key_file, section, key, value);
+    if ((int) g_key_file_load_from_file(key_file, filepath, G_KEY_FILE_NONE, NULL)==1) {
+        g_key_file_set_string(key_file, section, key, value);
+    }
 
     // Get the content of the config to a string...
     config = g_key_file_to_data(key_file, &length, &error);
     if (error == NULL) { // If everything is ok...
         outfile = fopen(filepath, "w");
         if (outfile != NULL) {
-            fwrite(config, sizeof(gchar), length, outfile);
+            fwrite(config, sizeof(gchar), (size_t) length, outfile);
             fclose(outfile);
         }
     }
 
     g_free(config);
+    g_free(key_file);
+
     if ((error != NULL) || (outfile == NULL)) {
         return -1;
     }
