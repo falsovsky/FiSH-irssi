d4c4517ea9f2065ebdfc9a4bc52ea81a60f7e5e8
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-02-08 19:30:51 +0000

    More testing

--- test/test.c
+++ test/test.c
@@ -5,10 +5,12 @@
 #include <assert.h>
 #include <unistd.h>
 
+static const char sample[] = "hide your brains";
+
 void RUN (void (*name)(fish2_t)) {
   fish2_t ctx;
   unlink("test.ini");
-  fish2_init(&ctx, "test.ini");
+  assert(fish2_init(&ctx, "test.ini") == 0);
   name(ctx);
   fish2_deinit(ctx);
 }
@@ -34,6 +36,8 @@ void test_has_key(fish2_t ctx) {
   assert(fish2_has_key(ctx, "freenet", "nick") == 0);
   assert(fish2_set_key(ctx, "freenet", "nick", "mininikey") == 0);
   assert(fish2_has_key(ctx, "freenet", "nick") != 0);
+  assert(fish2_unset_key(ctx, "freenet", "nick") == 0);
+  assert(fish2_has_key(ctx, "freenet", "nick") == 0);
 }
 
 void test_encrypt(fish2_t ctx) {
@@ -41,7 +45,7 @@ void test_encrypt(fish2_t ctx) {
 
   assert(fish2_validate_master_key(ctx, NULL) == 0);
   assert(fish2_set_key(ctx, "freenet", "nick", "mininikey") == 0);
-  assert(fish2_encrypt(ctx, "freenet", "nick", "hide your brains", text, sizeof(text)) == 0);
+  assert(fish2_encrypt(ctx, "freenet", "nick", sample, text, sizeof(text)) == 0);
 
   printf("[%s]\n", text);
 }
@@ -53,29 +57,34 @@ void test_roundtrip(fish2_t ctx) {
   assert(fish2_validate_master_key(ctx, NULL) == 0);
   assert(fish2_set_key(ctx, "freenet", "nick", "mininikey") == 0);
 
-  assert(fish2_encrypt(ctx, "freenet", "nick", "hide your brains", text, sizeof(text)) == 0);
+  assert(fish2_encrypt(ctx, "freenet", "nick", sample, text, sizeof(text)) == 0);
   assert(fish2_decrypt(ctx, "freenet", "nick", text, back, sizeof(back)) == 0);
 
   assert(strcmp(text, back));
-  assert(!strcmp("hide your brains", back));
+  assert(!strcmp(sample, back));
+}
+
+void test_mark(fish2_t ctx) {
+  char text[1024];
+
+  assert(fish2_validate_master_key(ctx, NULL) == 0);
+  assert(fish2_set_key(ctx, "freenet", "nick", "mininikey") == 0);
+
+  assert(fish2_mark_encryption(ctx, "freenet", "nick", sample, 0, text, sizeof(text)) == 0);
 }
 
 int main () {
-  {
-    fish2_t ctx;
-
-    int ret = fish2_init(&ctx, "test.ini");
-    printf("reting: %d\n", ret);
-
-    fish2_deinit(ctx);
+  int i;
+  int n = 10;
+  for (i = 0; i < n; ++i) {
+    RUN(test_nothing);
+    RUN(test_default_key);
+    RUN(test_set_master_key);
+    RUN(test_has_key);
+    RUN(test_encrypt);
+    RUN(test_roundtrip);
+    RUN(test_mark);
   }
-
-  RUN(test_nothing);
-  RUN(test_default_key);
-  RUN(test_set_master_key);
-  RUN(test_has_key);
-  RUN(test_encrypt);
-  RUN(test_roundtrip);
 
   return 0;
 }
