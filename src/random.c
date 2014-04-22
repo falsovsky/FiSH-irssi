c6d4a578223a702b923db576b208da66ea106ff0
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-04-22 23:33:54 +0100

    Fixes file descriptor leak in random_read

--- src/random.c
+++ src/random.c
@@ -6,9 +6,15 @@ int random_read (unsigned char* dest, size_t n)
 {
     FILE* fp = fopen("/dev/urandom", "rb");
 
+    if (fp == NULL) {
+        return -2;
+    }
+
     if (fread(dest, 1, n, fp) != n) {
+        fclose(fp);
         return -1;
     }
 
+    fclose(fp);
     return 0;
 }
