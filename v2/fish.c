e747ff6c61d83bf9d7a4139590af598077a96189
Hugo Peixoto <hugo.peixoto@gmail.com>
2016-01-21 03:36:58 +0000

    Fixes allocation size in fish functions

--- v2/fish.c
+++ v2/fish.c
@@ -26,8 +26,9 @@ char *fish_decrypt(const char* server, const char* who, const char* msg) {
   char *plaintext;
 
   if (strncmp(msg, "+OK ", 4) == 0) {
-    plaintext = malloc(strlen(msg) * 4 + 1);
     char *decrypted = my_decrypt_string(key, msg + 4);
+
+    plaintext = malloc(strlen(decrypted) + 1);
     sprintf(plaintext, "%s", decrypted);
     free(decrypted);
   } else
@@ -40,8 +41,9 @@ char *fish_decrypt(const char* server, const char* who, const char* msg) {
 char *fish_encrypt(const char* server, const char* who, const char* msg) {
   char *key = fish_getkey(server, who);
 
-  char *ciphertext = malloc(strlen(msg)*4 + 1 + 4);
   char *encrypted = my_encrypt_string(key, msg);
+
+  char *ciphertext = malloc(4 + strlen(encrypted) + 1);
   sprintf(ciphertext, "+OK %s", encrypted);
   free(encrypted);
 
