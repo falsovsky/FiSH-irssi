8bcfa4182e71abb82bc7b45b91b61c5a6e431923
Pedro de Oliveira <falsovsky@gmail.com>
2016-09-24 22:05:11 +0100

    Convert tabs to spaces and indent

--- src/password.c
+++ src/password.c
@@ -5,26 +5,26 @@
 
 void key_from_password(const char *a_password, char *a_key)
 {
-	unsigned char digest[256 / 8] = {0};	// SHA 256 uses 256/8 bytes.
-	int i;
+    unsigned char digest[256 / 8] = {0};	// SHA 256 uses 256/8 bytes.
+    int i;
 
-	SHA256((const unsigned char *)a_password, strlen(a_password), digest);
-	for (i = 0; i < 40872; i++) {
-		SHA256(digest, 32, digest);
-	}
+    SHA256((const unsigned char *)a_password, strlen(a_password), digest);
+    for (i = 0; i < 40872; i++) {
+        SHA256(digest, 32, digest);
+    }
 
-	memcpy(a_key, digest, sizeof(digest));
+    memcpy(a_key, digest, sizeof(digest));
 }
 
 void key_hash(const char *a_key, char *a_hash)
 {
-	unsigned char digest[256 / 8];
-	int i;
+    unsigned char digest[256 / 8];
+    int i;
 
-	memcpy(digest, a_key, sizeof(digest));
-	for (i = 0; i < 30752; i++) {
-		SHA256(digest, 32, digest);
-	}
+    memcpy(digest, a_key, sizeof(digest));
+    for (i = 0; i < 30752; i++) {
+        SHA256(digest, 32, digest);
+    }
 
-	memcpy(a_hash, digest, sizeof(digest));
+    memcpy(a_hash, digest, sizeof(digest));
 }
