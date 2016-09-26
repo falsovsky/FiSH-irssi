01080f8cb0b236ae017244b89c19d6bd77b08e37
Pedro de Oliveira <falsovsky@gmail.com>
2016-09-26 23:13:59 +0100

    Need to find a better way to fix this

--- src/common/blowfish.h
+++ src/common/blowfish.h
@@ -3,6 +3,14 @@
 #include <openssl/blowfish.h>
 #include "FiSH_version.h"
 
+#ifdef HAVE_STDINT
+    #include <stdint.h>
+#else
+    #ifdef HAVE_INTTYPES
+        #include <inttypes.h>
+    #endif
+#endif
+
 #define ZeroMemory(dest,count) memset((void *)dest, 0, count)
 
 int decrypt_string(const char *key, const char *str, char *dest, int len);
