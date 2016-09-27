2dd120f0843ea7bc977d0f169cac7be13aec7d89
Pedro de Oliveira <falsovsky@gmail.com>
2016-09-27 02:49:44 +0100

    Better algorithm for malloc!!

--- src/tests/test1.c
+++ src/tests/test1.c
@@ -2,38 +2,47 @@
 #include <assert.h>
 #include "blowfish.h"
 
-void *testClearKeyCrypt(const char *clearKey, const char *clearMessage, char *cryptedMessage) {
+void plainKeyCrypt(const char *plainKey, const char *clearMessage, char *cryptedMessage) {
     strcpy(cryptedMessage, "+OK ");
-    encrypt_string(clearKey, clearMessage, cryptedMessage + 4, strlen(clearMessage));
+    encrypt_string(plainKey, clearMessage, cryptedMessage + 4, strlen(clearMessage));
 }
 
-void testClearKeyDecrypt(const char *clearKey, const char *cryptedMessage, char *decryptedMessage) {
-    decrypt_string(clearKey, cryptedMessage + 4, decryptedMessage, strlen(cryptedMessage + 4));
+void plainKeyDecrypt(const char *plainKey, const char *cryptedMessage, char *decryptedMessage) {
+    decrypt_string(plainKey, cryptedMessage + 4, decryptedMessage, strlen(cryptedMessage + 4));
 }
 
-void testClearKey(const char *clearKey, const char *message, const char *assert) {
+void testPlainKey(const char *plainKey, const char *plainMessage, const char *assert) {
     char *cryptedMessage;
     char *decryptedMessage;
+    int plainSize = strlen(plainMessage);
+    int cryptedSize;
 
-    cryptedMessage = (char *) malloc(strlen(message) * 2);
-    testClearKeyCrypt(clearKey, message, cryptedMessage);
+    cryptedSize = (((((plainSize < 8 ? 8 : plainSize) + 8) & ~(0xf)) + ((plainSize / 8))) / 3 + 1) * 4 + 5;
+
+    cryptedMessage = (char *) malloc(cryptedSize);
+    plainKeyCrypt(plainKey, plainMessage, cryptedMessage);
     //printf("%s\n", cryptedMessage);
     assert(strcmp(cryptedMessage, assert) == 0);
 
-    decryptedMessage = (char *) malloc(strlen(cryptedMessage));
-    testClearKeyDecrypt(clearKey, cryptedMessage, decryptedMessage);
+    decryptedMessage = (char *) malloc(plainSize);
+    plainKeyDecrypt(plainKey, cryptedMessage, decryptedMessage);
     //printf("%s\n", decryptedMessage);
-    assert(strcmp(decryptedMessage, message) == 0);
+    assert(strcmp(decryptedMessage, plainMessage) == 0);
 
     free(cryptedMessage);
     free(decryptedMessage);
 }
 
+void testCryptedKey(const char *cryptedKey, const char *iniKey, const char *plainMessage, const char *assert) {
+
+}
+
 int main() {
-    testClearKey("morte666", "teste", "+OK 8qRoh/aBhUD0");
-    testClearKey("vivaobenficaglorioso",
+    testPlainKey("morte666", "teste", "+OK 8qRoh/aBhUD0");
+    testPlainKey("vivaobenficaglorioso",
         "ALGUM ATIVO, MASCULINO, QUE CURTA SER BEM MAMADO E COMER UM CU FIRME E REDONDINHO DE DESPORTISTA? SIGILO TOTAL",
         "+OK dLyvH1KCqym0s0hfc0n7Mew.ozzf/0MD06N1ywIA/.H4Qq.1Kbo3n0Kohah1O27E21tRcnt1nCgpe1mSyJN.unpoj.yvm0Z.elGgA.ceSTg.xmFga.tAROY.oilLa/9Tn4u0sSKkF1WSY340d54220eVDwk0vOG4K/rCAVe0");
-
+    //vivaobenficaglorioso
+    //testPlainKey("")
 }
 
